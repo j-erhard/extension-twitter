51efc0a30a1305a57ae705fc9b5ff6935a3a9ef5
ERHARD <julien.erhard@edu.univ-fcomte.fr>
2021-09-19 21:22:07 +0200

    Create background.js

---
+++ background.js
@@ -0,0 +1,9 @@
+chrome.runtime.onInstalled.addListener(function(){
+    alert("Merci d'avoir mis à jour ou installé mon extention !")
+});
+
+
+/*
+chrome.webNavigation.onCompleted.addListener(function(){
+    alert("Tu as chargé une nouvelle page!")
+});*/
