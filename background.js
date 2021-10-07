5cfabd000b552dec75ff346721c52249a6bd6664
Cyberooo <julien.erhard@edu.univ-fcomte.fr>
2021-10-07 19:46:49 +0200

    couleurs des tweets + récupération url + premier chargement

--- background.js
+++ background.js
@@ -1,10 +1,13 @@
+/*
 chrome.runtime.onInstalled.addListener(function(){
     alert("Merci d'avoir mis à jour ou installé mon extention !")
 });
+*/
 
 
 /*
 chrome.webNavigation.onCompleted.addListener(function(){
     alert("Tu as chargé une nouvelle page!")
-});*/
+});
+*/
 
--- content.js
+++ content.js
@@ -1,97 +1,107 @@
 // à mettre dans le manifest
 //"matches": ["<all_urls>"],
 
-// supprime tout
-// document.body.textContent = "";
-
-// test de code
-/*
-var header = document.createElement('h1');
-header.textContent = "This page has been eaten";
-document.body.appendChild(header);*/
-
-// met le background en rouge
-/*
-document.body.style.backgroundColor = "red";
-console.log("azerty");
-*/
-//miseEnAttente();
-
-function wait() {
-    console.log("...");
+/**
+ * Fonction qui affiche les tweets vérifiée dès leurs chargements
+ * @returns {Promise<void>}
+ */
+async function firstLoadTweet() {
+    var nbArticle = 0;
+    for (let i = 0; i < 100; i++) {
+        if (nbArticle <= 5) {
+            nbArticle = countArticle();
+            var x = await resolveXAndAfterX(100);
+        } else break;
+    }
+    affichageTweets();
 }
 
-function miseEnAttente(){
-    // var nbArticle = 0;
-    // for (let i = 0; i < 10; i++) {
-    //     if (nbArticle <= 6) {
-    //         nbArticle = changeColor();
-    //         setTimeout(wait, 10000); // on attend
-    //     }
-    // }
-    console.log("wesh les belges !");
+/**
+ * Function de mise en attente pendant X seconde
+ * @param x
+ * @returns {Promise<unknown>}
+ */
+function resolveXAndAfterX(x) {
+    return new Promise(resolve => {
+        setTimeout(() => {
+            resolve(x);
+        }, x);
+    });
 }
 
-// change la couleur de tous les background des articles en rouge
-function changeColor(){
+// Mets une couleur aléatoire sur les tweet
+function affichageTweets(){
     const articles = document.body.getElementsByTagName('article');
 
     //console.log(articles[0].style);
     for (const article of articles) {
+        // récupère l'url
         const url_tweet = getUrl(article.innerHTML);
-        //article.style = none;
-        article.style.background = "red";
-        article.style.background = '#'+(Math.random()*0xFFFFFF<<0).toString(16);
-        //console.log(article.innerHTML);
-        // span -> titre: id__wx7rpi4m65
 
-        ajoutBouton()
-    };
+        // Change l'aspect visuel de tweets
+        // distance x - distance y - dégradé - taille - (couleur r,g,b, transparence) - inset ou non
+        article.style.boxShadow = "none";
+        const X = Math.floor(Math.random() * 6);
+        if (X===0)
+            article.style.boxShadow = "0px 0px 10px 10px rgba(240, 25, 25, 0.8)";
+        else if (X===1)
+            article.style.boxShadow = "0px 0px 10px 10px rgba(240, 240, 25, 0.8)";
+        else if (X===2)
+            article.style.boxShadow = "0px 0px 10px 10px rgba(150, 150, 150, 0.8)";
+    }
+    return articles.length;
+}
+
+function countArticle(){
+    const articles = document.body.getElementsByTagName('article');
     return articles.length;
 }
 
 function getUrl(article) {
     var url = "https://twitter.com";
-    // match ne fonctionne pas
-    const model = "href=\"/TeamVitality/status/1440613329047678983\"";
-    var match = "href=\"";
-    var indexStatus = article.indexOf("/status/");
-    article = article.substring(indexStatus-25, indexStatus+50);
-    var index1 = article.indexOf("href=\"/");
-    article = article.substring(index1 + 6, index1 + 100);
-    var index2 = article.indexOf("\"");
-    article = article.substring(0,index2);
+    const INDEX_STATUS = article.indexOf("/status/");
+    article = article.substring(INDEX_STATUS-30, INDEX_STATUS+50);
+    const INDEX1 = article.indexOf("href=\"/");
+    article = article.substring(INDEX1 + 6, INDEX1 + 100);
+    const INDEX2 = article.indexOf("\"");
+    article = article.substring(0,INDEX2);
     url = url + article;
+    // Affichage en console
     console.log(url);
 
     return url;
 }
 
-// nul
-function ajoutBouton() {
+/**
+ * Ajout d'un bouton signaler
+ * @param article
+ */
+function ajoutBouton(article) {
     //ajout d'un bouton (pas à la bonne place)
-    if (!document.getElementsByClassName("bouton_report")) {
-        var input = document.createElement("input");
-        input.setAttribute("name", "report");
+    if (!(!document.getElementsByClassName("bouton_report123"))){
+        var input = document.createElement("bouton");
+        input.setAttribute("name", "bouton_report123");
         input.setAttribute("value", "value_input");
         input.setAttribute("class", "bouton_report");
         input.setAttribute("type", "button");
         article.appendChild(input);
+        console.log(document.getElementsByClassName("bouton_report123"));
+        console.log(article.innerHTML);
     }
 }
 
-document.addEventListener('readystatechange', () => miseEnAttente());
+document.addEventListener('readystatechange', () => firstLoadTweet());
 
 
-// S'enclenche dès que l'on scroll
+// S'enclenche dès que l'on scroll (pb: s'enclenche plusieurs fois pour un crant de la molette!!)
 var ticking = false;
 window.addEventListener('scroll', function(e) {
     if (!ticking) {
       window.requestAnimationFrame(function() {
-        var nbArticle = changeColor();
+        var nbArticle = affichageTweets();
         ticking = false;
       });
     }
     ticking = true;
-  });
+});
 
