476406acf34d42292be37959629b15b8a2353367
Cyberooo <julien.erhard@edu.univ-fcomte.fr>
2021-10-09 13:02:52 +0200

    modif bouton

--- README.md
+++ README.md
@@ -1,12 +1,11 @@
-<p align="center">
+<div align="center">
   <h3 align="center">Extension Twitter</h3>
 
   <p align="center">Notre projet a pour but de lutter contre la désinformation sur Twitter. Nous voulons donc résoudre ce problème, en créant une extension web qui permettrait aux utilisateurs Twitter, de pouvoir faire vérifier une information, qui selon eux, serait incertaine ou suspicieuse. L’utilisateur aurait également la possibilité de voir les tweets qui ont déjà été signalés. De plus, un site web serait créé afin de répertorier tous les tweets signalés et qui permettrait à des organismes et des particuliers de vérifier ceux ci.
     <br /><br />
     <a href="#installation"><b>INSTALLATION INSTRUCTIONS</b></a>
   </p>
-</p>
-
+</div>
 
 <!-- TABLE OF CONTENTS -->
 ## Table des matières
--- content.js
+++ content.js
@@ -29,11 +29,13 @@ function resolveXAndAfterX(x) {
     });
 }
 
-// Mets une couleur aléatoire sur les tweet
+/**
+ * Affichage complémentaire pour chaques tweets -> boutons + couleur
+ */
 function affichageTweets(){
+    // récupère tous les articles (tweets)
     const articles = document.body.getElementsByTagName('article');
-
-    //console.log(articles[0].style);
+    // boucles sur tous les articles (tweets)
     for (const article of articles) {
         // récupère l'url
         const url_tweet = getUrl(article.innerHTML);
@@ -53,15 +55,22 @@ function affichageTweets(){
         // Ajout d'un bouton
         ajoutBouton(article, url_tweet);
     }
-
-    return articles.length;
 }
 
+/**
+ * compte les articles (tweets)
+ * @returns {number}
+ */
 function countArticle(){
     const articles = document.body.getElementsByTagName('article');
     return articles.length;
 }
 
+/**
+ * récupère l'url d'un tweet
+ * @param article
+ * @returns {string}
+ */
 function getUrl(article) {
     var url = "https://twitter.com";
     const INDEX_STATUS = article.indexOf("/status/");
@@ -80,23 +89,22 @@ function getUrl(article) {
 /**
  * Ajout d'un bouton signaler
  * @param article
+ * @param url_tweet
  */
 function ajoutBouton(article, url_tweet) {
     //ajout d'un bouton
     var groupBoutons = article.getElementsByClassName('css-1dbjc4n r-1ta3fxp r-18u37iz r-1wtj0ep r-1s2bzr4 r-1mdbhws');
     for (const groupBouton of groupBoutons) {
-        // groupBouton.style.backgroundColor = "red";
-        console.log(groupBouton.childElementCount);
         groupBouton.parentElement.style.display = "inline-block";
-        // groupBouton.parentElement.style.verticalAlign = "middle";
         groupBouton.style.width = "70%";
         groupBouton.style.float = "left";
+        groupBouton.style.marginRight = "6%";
         if (groupBouton.parentElement.childElementCount <= 1) {
             var input = document.createElement("div");
             input.setAttribute("role", "button");
             input.setAttribute("tabindex", "0");
             input.setAttribute("class", "css-18t94o4 css-1dbjc4n r-1777fci r-bt1l66 r-1ny4l3l r-bztko3 r-lrvibr");
-            input.style.margin = "12px 0 10% 10%";
+            input.style.marginTop = "12px";
             input.style.backgroundColor="#0093f5";
             input.style.width = "20%";
             input.style.height = "0px";
@@ -109,32 +117,26 @@ function ajoutBouton(article, url_tweet) {
             groupBouton.parentElement.appendChild(input);
         }
     }
-
-    /*if (groupBouton.getElementsByClassName('css-1dbjc4n r-18u37iz r-1h0z5md bouton-report')){
-        console.log("wesh alors les belges!!!")
-        var input = document.createElement("bouton");
-        input.setAttribute("name", "bouton_report123");
-        input.setAttribute("value", "value_input");
-        input.setAttribute("class", "bouton_report");
-        input.setAttribute("type", "button");
-        article.appendChild(input);
-        console.log(document.getElementsByClassName("bouton_report123"));
-        console.log(article.innerHTML);
-    }*/
 }
 
-document.addEventListener('readystatechange', () => firstLoadTweet());
 
+/***************************************************************
+ *                   Écouteurs de la page web                  *
+ ***************************************************************/
+document.addEventListener('readystatechange', () => firstLoadTweet());
 
 // S'enclenche dès que l'on scroll (pb: s'enclenche plusieurs fois pour un crant de la molette!!)
 var ticking = false;
 window.addEventListener('scroll', function(e) {
     if (!ticking) {
       window.requestAnimationFrame(function() {
-        var nbArticle = affichageTweets();
+        affichageTweets();
         ticking = false;
       });
     }
     ticking = true;
 });
 
+/***************************************************************
+ *                   Récupération de données                   *
+ ***************************************************************/
