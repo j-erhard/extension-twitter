dc9eb4f102ae5ffa46442bbe3e6f3ecf82496161
Antoine JEAN <jenaraw91@gmail.com>
2021-11-24 21:59:22 +0100

    allow site web to api

--- api/index.js
+++ api/index.js
@@ -20,7 +20,7 @@ const swaggerUi = require("swagger-ui-express");
 
 //enable CORS
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*,http://localhost:3000/");
+    res.header("Access-Control-Allow-Origin", "*,http://localhost:3000/*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
     res.header("Access-Control-Allow-Credentials", "true");
