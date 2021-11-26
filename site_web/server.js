079e57f8e0bdfca0a3053e5379f0eb80393978d2
celialctnm <celia.leichtnam@gmail.com>
2021-11-26 10:52:47 +0100

    commit connexion

--- site_web/server.js
+++ site_web/server.js
@@ -35,8 +35,9 @@ app.set("view engine", "hbs");
 app.use(express.static(path.join(__dirname, "public")));
 
 app.get('/', function(req, res){
-    res.render('contact');
+    res.render('signin');
 });
+
 
 app.post('/contact', function(req, res, next){
     mailer.extend(app, {
