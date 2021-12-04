6bda3a858a9dbfa147cbbd84016f654213bee35b
Antoine JEAN <jenaraw91@gmail.com>
2021-12-04 17:05:59 +0100

    avec la bonne foonction dans

--- site_web/api_request/admin.request.js
+++ site_web/api_request/admin.request.js
@@ -1,12 +1,16 @@
-exports.findAllSignaledTweetOrderedByNbRequesDESC = async () =>{
-    let url = "http://localhost:8081/siteWeb/findAllSignaledTweetOrderedByNbRequesDESC"
-    let response = await fetch(url, {
-            method: 'GET',
+export async function getVerificationOfVerificatorById (url_tweet){
+    let url_api_request = "http://localhost:8081/verifie/getVerificationOfVerificatorById"
+
+    let response = await fetch(url_api_request, {
+            method: 'post',
             headers: {
                 'Accept': 'application/json',
                 'Content-Type': 'application/json'
                 //Origin: origin
             },
+            body: ({
+                url:url_tweet,
+            })
         },
         {mode: 'cors'})
         .then(result => result.json())
@@ -16,6 +20,3 @@ exports.findAllSignaledTweetOrderedByNbRequesDESC = async () =>{
 
     return (response)
 }
-
-
-
